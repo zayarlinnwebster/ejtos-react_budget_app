74d485a5a09fcd1d0471a150ddc59800b63e5e7c
zayarlinnwebster <zayarl513@gmail.com>
2023-05-09 04:26:50 -0400

    editable budget value

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,37 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget, remaining, expenses } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+    const setBudget = (newBudget) => {
+        if (newBudget > 20000) {
+            alert("The value cannot exceed remaining funds  £" + remaining);
+            return;
+        } else if (newBudget < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+            return;
+        }
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget,
+        });
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+                <input
+                style={{width: 70 + '%'}}
+                required='required'
+                type='number'
+                id='budget'
+                step='10'
+                value={budget}
+                onChange={(event) => setBudget(event.target.value)}>
+            </input></span>
         </div>
     );
 };
